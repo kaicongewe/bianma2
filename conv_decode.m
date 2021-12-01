633e9577b0f7b6738323d52f34c280b57478bc86
Daniel Fang <76408862+Hoark@users.noreply.github.com>
2021-12-01 13:55:32 +0800

    卷积译码模块

---
+++ conv_decode.m
@@ -0,0 +1,6 @@
+function output = conv_decode(input, A)
+%Use Viterbi to decode the input sequence, return decoded sequence
+%Inputs: input = inputted bit sequence
+%        A = convolution encoding projection matrix (See conv_encode)
+
+end
